301658458a61983859e76945e6ea1e2bb1ba10e4
hasan-abir <contact.hasanabir@gmail.com>
2024-02-10 19:48:31 +0600

    Expand text and related bugs fixed

--- frontend/src/components/AnimatedRichText.tsx
+++ frontend/src/components/AnimatedRichText.tsx
@@ -37,7 +37,7 @@ const portableTextComponents: PortableTextComponents = {
             return (
               <p
                 key={i}
-                className="line mr-[0.2rem] text-xl opacity-0 translate-x-[3rem]"
+                className="line mr-[0.2rem] text-xl opacity-0 translate-x-[3rem] will-change-transform"
               >
                 {(line.includes("°") &&
                   line.match(/[^°]+/g)?.map((phrase, j) => {
@@ -73,6 +73,9 @@ const AnimatedRichText = ({ body }: Props) => {
       const triggerEl = document.querySelector(
         ".next-line-btn"
       ) as HTMLButtonElement;
+      const expandBtn = document.querySelector(
+        ".expand-text-btn"
+      ) as HTMLButtonElement;
 
       let i = 0;
 
@@ -82,50 +85,94 @@ const AnimatedRichText = ({ body }: Props) => {
         timeline = timeline.add(`el-${i}`).to(targetEl, {
           opacity: 1,
           x: 0,
-          duration: 0.3,
+          duration: 1,
+          ease: "expo.out",
           onComplete: () => {
             timeline.pause();
+            targetEl.style.willChange = "auto";
           },
         });
 
         i++;
       }
 
-      const scrollToLine = () => {
-        container.current?.scrollTo(
-          0,
-          targets[currentIndex].offsetTop - container.current?.offsetTop
-        );
+      const expandPrevLines = () => {
+        let prevLine = currentIndex - 3 < 0 ? 0 : currentIndex - 3;
 
         gsap.to(container.current, {
-          height: targets[currentIndex].offsetHeight,
+          height:
+            targets[prevLine].offsetHeight +
+            targets[prevLine + 1].offsetHeight +
+            targets[prevLine + 2].offsetHeight,
           duration: 0.2,
+          onComplete: () => {
+            container.current?.scrollTo({
+              top: targets[prevLine].offsetTop - container.current?.offsetTop,
+              behavior: "smooth",
+            });
+          },
         });
-
-        timeline.play(`el-${currentIndex}`);
-        currentIndex++;
       };
 
-      scrollToLine();
+      const goToLine = (i: number) => {
+        gsap.to(container.current, {
+          height: targets[i].offsetHeight,
+          duration: 0.2,
+          onComplete: () => {
+            container.current?.scrollTo({
+              top: targets[i].offsetTop - container.current?.offsetTop,
+              behavior: "smooth",
+            });
+          },
+        });
 
-      let onClick: () => void = () => {};
+        timeline.play(`el-${i}`);
+      };
+
+      goToLine(currentIndex);
+      currentIndex++;
+
+      let onNextLine: (e: MouseEvent | KeyboardEvent) => void = () => {};
+      let onExpandText: (e: MouseEvent | KeyboardEvent) => void = () => {};
 
       if (contextSafe) {
-        onClick = contextSafe(() => {
-          if (currentIndex < targets.length) {
-            scrollToLine();
-          }
-          if (currentIndex === targets.length) {
+        onNextLine = contextSafe((e) => {
+          if ((e as KeyboardEvent).key && (e as KeyboardEvent).key !== "Enter")
+            return null;
+
+          goToLine(currentIndex);
+
+          if (currentIndex < targets.length - 1) {
+            currentIndex++;
+          } else {
             triggerEl.textContent = "Next page";
             triggerEl.disabled = true;
+            document.body.removeEventListener("keyup", onNextLine);
+            currentIndex++;
           }
+        });
+
+        onExpandText = contextSafe((e) => {
+          if (
+            (e as KeyboardEvent).key &&
+            (e as KeyboardEvent).key !== "Backspace"
+          )
+            return null;
+
+          expandPrevLines();
         });
       }
 
-      triggerEl.addEventListener("click", onClick);
+      triggerEl.addEventListener("click", onNextLine);
+      document.body.addEventListener("keyup", onNextLine);
+      expandBtn.addEventListener("click", onExpandText);
+      document.body.addEventListener("keyup", onExpandText);
 
       return () => {
-        triggerEl.removeEventListener("click", onClick);
+        triggerEl.removeEventListener("click", onNextLine);
+        document.body.removeEventListener("keyup", onNextLine);
+        expandBtn.removeEventListener("click", onExpandText);
+        document.body.removeEventListener("keyup", onExpandText);
       };
     },
     { scope: container }
@@ -139,7 +186,10 @@ const AnimatedRichText = ({ body }: Props) => {
       >
         <PortableText value={body} components={portableTextComponents} />
       </div>
-      <button className="next-line-btn mt-4 underline">Next line</button>
+      <div className="flex mt-4">
+        <button className="next-line-btn underline mr-4">Next line</button>
+        <button className="expand-text-btn underline">Expand</button>
+      </div>
     </div>
   );
 };
