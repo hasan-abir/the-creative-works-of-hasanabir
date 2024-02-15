5adf4c31b65163720ebff5d8a28447a4e7a31d14
hasan-abir <contact.hasanabir@gmail.com>
2024-02-15 11:58:04 +0600

    Progress bar

--- frontend/src/components/AnimatedRichText.tsx
+++ frontend/src/components/AnimatedRichText.tsx
@@ -303,6 +303,14 @@ const AnimatedRichText = ({
       }}
       tabIndex={0}
     >
+      <div className="w-full mb-4 bg-neutral-300 dark:bg-neutral-800 rounded-full overflow-x-hidden opacity-25">
+        <div
+          className="w-full h-2 bg-neutral-600 transition-transform origin-left"
+          style={{
+            transform: `scaleX(${currentIndex / (lines.current?.length || 1)}`,
+          }}
+        ></div>
+      </div>
       <div
         className="rich-text-container overflow-x-hidden h-12 w-full resize-y mb-24"
         ref={container}
