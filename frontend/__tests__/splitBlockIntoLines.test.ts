dcc5ce72d6095612c0a6488b8e31f322891b915b
hasan-abir <contact.hasanabir@gmail.com>
2024-02-19 11:56:49 +0600

    Support for screenplay format

--- frontend/__tests__/splitBlockIntoLines.test.ts
+++ frontend/__tests__/splitBlockIntoLines.test.ts
@@ -31,10 +31,9 @@ describe("splitBlockIntoLines: ", () => {
   });
 
   it("separates by .?! excluding abbreviations", () => {
-    block = `${block} Mr. Smith is flavorful. Mrs. Smith is more.`;
+    block = `${block} Mr. Smith is flavorful. Is Mrs. Smith here?`;
 
     const lines = splitBlockIntoLines(block);
-    console.log(lines);
 
     expect(lines.length).toBe(6);
     expect(lines[0]).toBe("Lorem ipsum dolor.");
@@ -42,6 +41,25 @@ describe("splitBlockIntoLines: ", () => {
     expect(lines[2]).toBe("Nullam eleifend sollicitudin tortor?");
     expect(lines[3]).toBe("Sed sed mollis mi.");
     expect(lines[4]).toBe("Mr. Smith is flavorful.");
-    expect(lines[5]).toBe("Mrs. Smith is more.");
+    expect(lines[5]).toBe("Is Mrs. Smith here?");
+  });
+
+  it("separates by square brackets and punctuation end quote", () => {
+    block = `${block} [Are you "experienced"? Well, I am.] John [angrily]: Get back here! "What a day!" "What a henious day." "Is that so?" I'm gonna get you`;
+
+    const lines = splitBlockIntoLines(block);
+
+    expect(lines.length).toBe(11);
+    expect(lines[0]).toBe("Lorem ipsum dolor.");
+    expect(lines[1]).toBe("Etiam quis ullamcorper nunc!");
+    expect(lines[2]).toBe("Nullam eleifend sollicitudin tortor?");
+    expect(lines[3]).toBe("Sed sed mollis mi.");
+    expect(lines[4]).toBe(`[Are you "experienced"?`);
+    expect(lines[5]).toBe("Well, I am.]");
+    expect(lines[6]).toBe("John [angrily]: Get back here!");
+    expect(lines[7]).toBe(`"What a day!"`);
+    expect(lines[8]).toBe(`"What a henious day."`);
+    expect(lines[9]).toBe(`"Is that so?"`);
+    expect(lines[10]).toBe("I'm gonna get you");
   });
 });
--- frontend/src/utils/splitBlockIntoLines.ts
+++ frontend/src/utils/splitBlockIntoLines.ts
@@ -1,5 +1,9 @@
 const splitBlockIntoLines = (block: string): string[] => {
-  return block.match(/(?<!\S)(?:Mr\.|Mrs\.|[^.?!])*(?:[.?!](?=\s|$)|$)/g) || [];
+  return (
+    block.match(
+      /(?<!\S)(?:Mr\.|Mrs\.|[^.?!])*(?:[.?!]|\.\]|\.\"|\?\"|\!\"|$)(?=\s|$)/g
+    ) || []
+  );
 };
 
 export default splitBlockIntoLines;
