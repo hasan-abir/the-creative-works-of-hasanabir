21d8828abf47c9576800a760e51a5cd141230f27
hasan-abir <contact.hasanabir@gmail.com>
2024-03-04 11:25:18 +0600

    Button clicks tested in E2E

--- frontend/cypress/e2e/storyPage.cy.ts
+++ frontend/cypress/e2e/storyPage.cy.ts
@@ -19,6 +19,57 @@ describe("Story Page", () => {
     cy.get(".line:first").contains("The beginning");
     cy.get(".line:first").should("be.visible");
   });
+  it("should navigate lines and pages properly", () => {
+    const firstStory: Story = stories.list[0];
+    cy.visit(`http://localhost:3000/stories/${firstStory.slug.current}/1`);
+    cy.wait(1000);
+    cy.get(".line").eq(0).should("be.visible");
+    cy.get(".line").eq(0).contains("The beginning");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.wait(1000);
+    cy.get(".line").eq(1).should("be.visible");
+    cy.get(".line")
+      .eq(1)
+      .contains("I’d like to honor an event REALLY, if God allows me to.");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.wait(1000);
+    cy.get(".line").eq(2).should("be.visible");
+    cy.get(".line").eq(2).contains("His boss will be mad at the meeting.");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.wait(1000);
+    cy.get(".line").eq(3).should("be.visible");
+    cy.get(".line").eq(3).contains("You don't need 20 captains.");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.wait(1000);
+    cy.get(".line").eq(4).should("be.visible");
+    cy.get(".line").eq(4).contains("Every child likes an ice cream.");
+    cy.get(`[data-testid="next-page-link"]`).should("be.visible");
+    cy.get(`[data-testid="next-page-link"]`).trigger("click");
+    cy.url().should("include", `/stories/${firstStory.slug.current}/2`);
+    cy.wait(1000);
+    cy.get(".line").eq(0).should("be.visible");
+    cy.get(".line").eq(0).contains("The middeling");
+    cy.get(`[data-testid="prev-page-link"]`).should("be.visible");
+    cy.get(`[data-testid="prev-page-link"]`).trigger("click");
+    cy.url().should("include", `/stories/${firstStory.slug.current}/1`);
+    cy.wait(1000);
+    cy.get(".line").eq(4).should("be.visible");
+    cy.get(`[data-testid="to-top-btn"]`).trigger("click");
+    cy.wait(1000);
+    cy.get(".line").eq(0).should("be.visible");
+  });
+  it("should indicate the start and end of a story properly", () => {
+    const firstStory: Story = stories.list[0];
+    cy.visit(`http://localhost:3000/stories/${firstStory.slug.current}/1`);
+    cy.wait(1000);
+    cy.get("p").eq(7).contains("The Start");
+    cy.visit(`http://localhost:3000/stories/${firstStory.slug.current}/3`);
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.get(`[data-testid="next-line-btn"]`).trigger("click");
+    cy.get("p").eq(7).contains("The End");
+  });
   it("should display not found", () => {
     cy.visit("http://localhost:3000/stories/story1/4", {
       failOnStatusCode: false,
