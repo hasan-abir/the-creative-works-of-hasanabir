7ed934e83a03a7b9be33779c5f126b180b2db41a
hasan-abir <contact.hasanabir@gmail.com>
2024-08-19 08:45:59 +0600

    Typo fix

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
     <div className="page-container">
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="font-bold text-3xl sm:text-6xl text-center mb-4 sm:mb-8">
-          Page not found (neither its meaning)
+          Page not found (nor its meaning)
         </h1>
         <CTALink href="/" text="Back to Home" />
       </div>
