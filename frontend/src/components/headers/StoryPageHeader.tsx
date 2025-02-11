bbaa9df5c4504c2ac6743161b74136fe2617ff20
hasan-abir <contact.hasanabir@gmail.com>
2025-02-11 15:49:48 +0600

    Add page navigation dropdown to StoryPageHeader

--- frontend/src/components/headers/StoryPageHeader.tsx
+++ frontend/src/components/headers/StoryPageHeader.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import { NavArrowLeft } from "iconoir-react";
 import BackNav from "@/components/BackNav";
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
 
 interface Props {
   title: string;
@@ -10,6 +14,12 @@ interface Props {
 }
 
 const StoryPageHeader = ({ title, slug, page, pageCount }: Props) => {
+  const router = useRouter();
+
+  const jumpToPage = useCallback((pageNo: number) => {
+    router.push(`/stories/${slug}/${pageNo}`);
+  }, []);
+
   return (
     <header className="flex gap-2 justify-between items-center mb-4 sm:mb-6">
       <BackNav
@@ -29,7 +39,10 @@ const StoryPageHeader = ({ title, slug, page, pageCount }: Props) => {
         ]}
       />
       <p className="text-center sm:text-xl ">
-        <strong>{title}</strong> written by{" "}
+        <Link href={"/stories/" + slug}>
+          <strong>{title}</strong>
+        </Link>{" "}
+        written by{" "}
         <Link
           href="https://hasanabir.netlify.app/"
           target="_blank"
@@ -40,7 +53,20 @@ const StoryPageHeader = ({ title, slug, page, pageCount }: Props) => {
       </p>
 
       <p className="sm:text-xl whitespace-nowrap">
-        {page} of {pageCount}
+        <select
+          className="bg-light-50 dark:bg-dark-50 text-dark-50 dark:text-light-50 focus:ring-none focus:outline-none"
+          defaultValue={page}
+          onChange={(e) =>
+            jumpToPage(parseInt((e.target as HTMLSelectElement).value))
+          }
+        >
+          {Array.from({ length: pageCount }).map((val, index) => (
+            <option key={index} value={index + 1}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
+        of {pageCount}
       </p>
     </header>
   );
