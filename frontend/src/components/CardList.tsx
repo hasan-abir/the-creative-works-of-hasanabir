ce5fd8da5d192039a49bcefc342b6d7e365de4d9
hasan-abir <contact.hsanabir@gmail.com>
2026-02-27 19:16:38 +0600

    hide sides with bars

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -10,8 +10,12 @@ const CardList = ({ children, heading }: Props) => {
   return (
     <section>
       <h1 className={headingFont.className + " big-heading"}>{heading}</h1>
-      <div className="flex mb-12 pb-12 pt-6 px-12 overflow-x-auto">
-        {children}
+      <div className="relative">
+        <div className="flex overflow-x-auto mb-12 pb-12 pt-6 px-12">
+          {children}
+        </div>
+        <div className="absolute left-0 top-0 bg-light-100 w-12 h-full bg-to-nothing-left"></div>
+        <div className="absolute right-0 top-0 bg-light-100 w-12 h-full bg-to-nothing-right"></div>
       </div>
     </section>
   );
