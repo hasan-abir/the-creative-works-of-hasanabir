9011f205692c580aa850648a9f20914558a4da60
hasan-abir <contact.hsanabir@gmail.com>
2026-03-09 14:11:27 +0600

    hiding carousel btns

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ReactNode, useCallback, useState, useRef } from "react";
+import React, {
+  ReactNode,
+  useCallback,
+  useState,
+  useRef,
+  useEffect,
+} from "react";
 import { headingFont } from "@/utils/fonts";
 import { icons } from "@/utils/icons";
 import CTABtn from "@/components/CTABtn";
@@ -11,6 +17,9 @@ interface Props {
 }
 
 const CardList = ({ children, heading }: Props) => {
+  const [startOfCarousel, setStartOfCarousel] = useState(true);
+  const [endOfCarousel, setEndOfCarousel] = useState(true);
+
   const scrollRef = useRef<HTMLDivElement | null>(null);
 
   const scrollCarousel = useCallback((left: boolean = true) => {
@@ -31,6 +40,18 @@ const CardList = ({ children, heading }: Props) => {
     });
   }, []);
 
+  const onScroll = useCallback((el: HTMLDivElement) => {
+    const percentScrolled =
+      ((el.scrollLeft + el.clientWidth) / el.scrollWidth) * 100;
+
+    setStartOfCarousel(el.scrollLeft < 64);
+    setEndOfCarousel(percentScrolled > 95);
+  }, []);
+
+  useEffect(() => {
+    scrollRef.current && onScroll(scrollRef.current);
+  }, []);
+
   return (
     <section>
       <h1 className={headingFont.className + " big-heading"}>{heading}</h1>
@@ -38,23 +59,37 @@ const CardList = ({ children, heading }: Props) => {
         <div
           className="carousel-track flex overflow-x-auto mb-12 pb-12 pt-6 px-12"
           ref={scrollRef}
+          onScroll={(e) => onScroll(e.target as HTMLDivElement)}
         >
           {children}
         </div>
         <div className="absolute left-0 top-0 bg-light-100 h-full bg-to-nothing-left flex items-center">
           <CTABtn
             primary={false}
-            extraClasses="h-[150px] ml-2 text-white"
+            extraClasses={
+              "h-[150px] ml-2 text-white transition-opacity" +
+              (startOfCarousel
+                ? " opacity-0 cursor-default"
+                : "opacity-100 cursor-pointer")
+            }
             onClick={() => scrollCarousel(false)}
           >
             <icons.PreviousIcon />
           </CTABtn>
         </div>
-
-        <div className="absolute right-0 top-0 bg-light-100 h-full bg-to-nothing-right flex items-center">
+        <div
+          className={
+            "absolute right-0 top-0 bg-light-100 h-full bg-to-nothing-right flex items-center"
+          }
+        >
           <CTABtn
             primary={false}
-            extraClasses="h-[150px] mr-2 text-white"
+            extraClasses={
+              "h-[150px] mr-2 text-white transition-opacity" +
+              (endOfCarousel
+                ? " opacity-0 cursor-default"
+                : "opacity-100 cursor-pointer")
+            }
             onClick={() => scrollCarousel()}
           >
             <icons.NextIcon />
