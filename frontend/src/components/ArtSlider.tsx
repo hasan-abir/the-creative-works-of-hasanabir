91813544d4bb7620ab01d5fdc3b1897abf2b4fdb
hasan-abir <contact.hsanabir@gmail.com>
2026-06-15 12:35:44 +0600

    defining variables for slider part 2

--- frontend/src/components/ArtSlider.tsx
+++ frontend/src/components/ArtSlider.tsx
@@ -6,63 +6,119 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import Draggable from "gsap/Draggable";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { NullLiteral } from "typescript";
 
 interface Props {}
 
 const ArtSlider = ({}: Props) => {
   const [active, setActive] = useState<number>(5);
   const container = useRef<HTMLDivElement>(null);
+  const snapTime = useRef<((valueToSnap: number) => number) | null>(null);
+  const seamlessLoop = useRef<gsap.core.Timeline | undefined | null>(null);
+  const iteration = useRef<number | null>(null);
+  const trigger = useRef<globalThis.ScrollTrigger | null>(null);
+  const wrap = useRef<
+    ((iterationDelta: number, scrollTo: number) => void) | null
+  >(null);
+  const scroll = useRef<number | undefined | null>(null);
 
   const { contextSafe } = useGSAP(
     () => {
       gsap.registerPlugin(useGSAP, ScrollTrigger, Draggable);
 
-      let iteration = 0;
+      iteration.current = 0;
       const stagger = 0.1;
-      const snapTime = gsap.utils.snap(stagger);
+      snapTime.current = gsap.utils.snap(stagger);
       const slides = gsap.utils.toArray(".sliders .slide");
       const playhead = { offset: 0 };
-      const seamlessLoop = buildSeamlessLoop(slides, stagger, animFunc);
-      if (seamlessLoop) {
-        const wrapTime = gsap.utils.wrap(0, seamlessLoop.duration());
-        const wrap = (iterationDelta: number, scrollTo: number) => {
-          iteration += iterationDelta;
-          trigger.scroll(scrollTo);
-          trigger.update();
-        };
-        const scrub = gsap.to(playhead, {
-            offset: 0,
-            onUpdate() {
-              seamlessLoop.time(wrapTime(playhead.offset));
-            },
-            duration: 0.5,
-            ease: "power3",
-            paused: true,
-          }),
-          trigger = ScrollTrigger.create({
-            start: 0,
-            onUpdate(self) {
-              let scroll = self.scroll();
-              if (scroll > self.end - 1) {
-                wrap(1, 2);
-              } else if (scroll < 1 && self.direction < 0) {
-                wrap(-1, self.end - 2);
-              } else {
-                scrub.vars.offset =
-                  (iteration + self.progress) * seamlessLoop.duration();
-                scrub.invalidate().restart();
-              }
-            },
-            end: "+=3000",
-            pin: ".gallery",
-          });
-      }
+      seamlessLoop.current = buildSeamlessLoop(slides, stagger, animFunc);
+
+      if (!seamlessLoop.current || !iteration.current) return;
+
+      const wrapTime = gsap.utils.wrap(0, seamlessLoop.current.duration());
+      const wrap = (iterationDelta: number, scrollTo: number) => {
+        if (!iteration.current || !trigger.current) return;
+        iteration.current += iterationDelta;
+        trigger.current.scroll(scrollTo);
+        trigger.current.update();
+      };
+      const scrub = gsap.to(playhead, {
+        offset: 0,
+        onUpdate() {
+          if (!seamlessLoop.current) return;
+          seamlessLoop.current.time(wrapTime(playhead.offset));
+        },
+        duration: 0.5,
+        ease: "power3",
+        paused: true,
+      });
+      trigger.current = ScrollTrigger.create({
+        start: 0,
+        onUpdate(self) {
+          if (!seamlessLoop.current || !iteration.current) return;
+
+          scroll.current = self.scroll();
+
+          if (!scroll.current) return;
+
+          if (scroll.current > self.end - 1) {
+            wrap(1, 2);
+          } else if (scroll.current < 1 && self.direction < 0) {
+            wrap(-1, self.end - 2);
+          } else {
+            scrub.vars.offset =
+              (iteration.current + self.progress) *
+              seamlessLoop.current.duration();
+            scrub.invalidate().restart();
+          }
+        },
+        end: "+=3000",
+        pin: ".slider-container",
+      });
+
+      ScrollTrigger.addEventListener("scrollEnd", () =>
+        scrollToOffset(scrub.vars.offset),
+      );
 
       gsap.set(".slider .slide", { xPercent: 400, opacity: 0, scale: 0 });
-
-      // animFunc(".slider .slide");
     },
     { scope: container },
+  );
+
+  const scrollToOffset = contextSafe(
+    useCallback((offset: number) => {
+      if (
+        !snapTime.current ||
+        !seamlessLoop.current ||
+        !iteration.current ||
+        !trigger.current ||
+        !scroll.current ||
+        !wrap.current
+      )
+        return;
+
+      let snappedTime = snapTime.current(offset);
+      const progress =
+        (snappedTime - seamlessLoop.current.duration() * iteration.current) /
+        seamlessLoop.current.duration();
+      scroll.current = progressToScroll(progress);
+      if (progress >= 1 || progress < 0) {
+        return wrap.current(Math.floor(progress), scroll.current as number);
+      }
+      trigger.current.scroll(scroll.current as number);
+    }, []),
+  );
+
+  const progressToScroll = contextSafe(
+    useCallback((progress: number) => {
+      if (!trigger.current) return;
+
+      return gsap.utils.clamp(
+        1,
+        trigger.current.end - 1,
+        gsap.utils.wrap(0, 1, progress) * trigger.current.end,
+      );
+    }, []),
   );
 
   const animFunc = contextSafe(
@@ -102,11 +158,9 @@ const ArtSlider = ({}: Props) => {
       const loopTime = (items.length + overlap) * stagger + 1;
       const rawSequence = gsap.timeline({ paused: true });
       const seamlessLoop = gsap.timeline({
-        // this merely scrubs the playhead of the rawSequence so that it appears to seamlessly loop
         paused: true,
-        repeat: -1, // to accommodate infinite scrolling/looping
+        repeat: -1,
         onRepeat() {
-          // works around a super rare edge case bug that's fixed GSAP 3.6.1
           this._time === this._dur && (this._tTime += this._dur - 0.01);
         },
       });
@@ -115,7 +169,6 @@ const ArtSlider = ({}: Props) => {
       let i = null;
       let index = null;
 
-      // now loop through and create all the animations in a staggered fashion. Remember, we must create EXTRA animations at the end to accommodate the seamless looping.
       for (i = 0; i < l; i++) {
         index = i % items.length;
         timeToAnim = i * stagger;
@@ -147,7 +200,10 @@ const ArtSlider = ({}: Props) => {
     [],
   );
   return (
-    <section className="overflow-hidden h-[430px] relative" ref={container}>
+    <section
+      className="slider-container overflow-hidden h-[430px] relative"
+      ref={container}
+    >
       <div
         className="slider flex justify-center items-end w-max absolute top-0 left-1/2 translate-x-[-50%]"
         onClick={animFunc}
